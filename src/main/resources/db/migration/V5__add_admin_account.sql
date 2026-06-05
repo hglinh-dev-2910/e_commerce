7153a4a7baadd974533e2ca827af863a2908e142
hglinh-dev-2910 <hglinh.dev@gmail.com>
2026-06-05 18:05:53 +0700

    update: add demo admin account

---
+++ src/main/resources/db/migration/V5__add_admin_account.sql
@@ -0,0 +1,14 @@
+INSERT INTO users (
+    username,
+    email,
+    password,
+    role,
+    created_at
+) VALUES (
+    'admin',
+    'admin01@gmail.com',
+    '$2a$12$cyFyI62THKHXlBqbftnduuxYW4FdszpWxdzWSvZwKV0rc.j/GvWym',
+    'ADMIN',
+    NOW()
+
+)
